f0c383261d4ea8d7d7c84f814ec4d8eb8cbf88d4
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-05-26 20:51:34 -0300

    Criação da Classe Perguntas()
    * objeto com 3 métodos porém até o momento só consegui fazer 1 funcionar que é o das perguntas > armazenar os dados em uma lista > exibir a lista com data e hora
    *ainda preciso encontrar uma maneira de encerrar o programa através de um método dentro do objeto e salvar o arquivo em .csv também através de um método dentro do objeto

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,36 @@
-# def idade_candidato(idade_candidato):
-#     idade_dos_candidatos = [] #Lista para receber a idade dos candidatos 
-#     idade_candidato = input('Digite a idade do entrevistado')
-#     while idade_candidato.isnumeric()==False or int(idade_candidato) > 00:
-#         idade_candidato = input('Digite a idade do entrevistado')
-#         idade_dos_candidatos.append(idade_candidato)
-#         if idade_candidato.isnumeric == 00:
-#             print(idade_dos_candidatos)
-#             break
+from datetime import datetime #importa a biblioteca responsavel por inserir data e hora
 
+class Perguntas(): 
+    def obter_resposta(self):  #método criado para fazer o questionamento das 6 perguntas e registrar data e hora
+        self.respostas = []
+        idade = input('Insira sua idade: ')        
+        genero = input('Qual é o seu gênero? ')
+        print('\n Para responder o questionário utilize : Sim (1), Não (2), Não sei responder (3)?')
+        p1 = input('\nCompraria um carro pela internet? ')
+        p2 = input('\nSe o preço do carro fosse 15% abaixo do valor de mercado, você compraria? ')
+        p3 = input('\nTem intenção de comprar um carro nos próximos 6 meses? ')
+        p4 = input('\nGostaria do serviço de despachante na compra do seu carro? ')        
+        self.respostas.append([idade])
+        self.respostas.append([genero]) 
+        self.respostas.append([p1])
+        self.respostas.append([p2])
+        self.respostas.append([p3])
+        self.respostas.append([p4])
+        hora_atual = datetime.now() #Registra o horário
+        data_e_hora_atual = hora_atual.strftime('%d/%m/%Y %H:%M') #registra a data e implementa o horário da variável 'hora atual'.
+        self.respostas.append(data_e_hora_atual)
+        print(self.respostas)  #O self é usado para acessar os atributos e métodos da instância dentro da própria classe.
+        
+    def responder_pesquisa(self): #metodo criado para verificar a entrada do usuario
+        while True:
+            resposta = self.obter_resposta()
+            if resposta == [00]:
+                break
+            self.respostas.append(resposta)
+        print(self.respostas)                  
+    
+    def save_csv(self): #metodo criado para salvar o arquivo em csv
+        return
+    
+questionario = Perguntas()
+questionario.obter_resposta()
