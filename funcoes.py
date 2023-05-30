c17a334a96e92a1b7c3d8aae057727bc1f412b27
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-05-30 11:45:01 -0300

    Modularização
    Programação orientada a objetos

--- funcoes.py
+++ funcoes.py
@@ -1,21 +1,32 @@
 from datetime import datetime #importa a biblioteca responsavel por inserir data e hora
 
 class Perguntas(): 
+    
+    respostas = []
+    
     def obter_resposta(self):  #método criado para fazer o questionamento das 6 perguntas e registrar data e hora
-        self.respostas = []
-        idade = input('Insira sua idade: ')        
-        genero = input('Qual é o seu gênero? ')
-        print('\n Para responder o questionário utilize : Sim (1), Não (2), Não sei responder (3)?')
-        p1 = input('\nCompraria um carro pela internet? ')
-        p2 = input('\nSe o preço do carro fosse 15% abaixo do valor de mercado, você compraria? ')
-        p3 = input('\nTem intenção de comprar um carro nos próximos 6 meses? ')
-        p4 = input('\nGostaria do serviço de despachante na compra do seu carro? ')        
-        self.respostas.append([idade])
-        self.respostas.append([genero]) 
-        self.respostas.append([p1])
-        self.respostas.append([p2])
-        self.respostas.append([p3])
-        self.respostas.append([p4])
+        self.idade = input('Insira sua idade: ')        
+        self.genero = input('Qual é o seu gênero? ')
+        
+        while True:
+            try:                
+                print('\n Para responder o questionário utilize : Sim (1), Não (2), Não sei responder (3)?')
+                self.p1 = input('\nCompraria um carro pela internet? ')
+                self.p2 = input('\nSe o preço do carro fosse 15% abaixo do valor de mercado, você compraria? ')
+                self.p3 = input('\nTem intenção de comprar um carro nos próximos 6 meses? ')
+                self.p4 = input('\nGostaria do serviço de despachante na compra do seu carro? ')
+                if self.p1 and self.p2 and self.p3 and self.p4 < '1' or self.p1 and self.p2 and self.p3 and self.p4 > '3':
+                    raise ValueError  #função utilizada para levantar uma exceção explicitamente em seu código
+            except ValueError:
+                print('Insira um numero entre 1 e 3 ')        
+        
+    def add_lista(self):
+        self.respostas.append([self.idade])
+        self.respostas.append([self.genero]) 
+        self.respostas.append([self.p1])
+        self.respostas.append([self.p2])
+        self.respostas.append([self.p3])
+        self.respostas.append([self.p4])
         hora_atual = datetime.now() #Registra o horário
         data_e_hora_atual = hora_atual.strftime('%d/%m/%Y %H:%M') #registra a data e implementa o horário da variável 'hora atual'.
         self.respostas.append(data_e_hora_atual)
@@ -30,7 +41,6 @@ class Perguntas():
         print(self.respostas)                  
     
     def save_csv(self): #metodo criado para salvar o arquivo em csv
-        return
+        return 
     
-questionario = Perguntas()
-questionario.obter_resposta()
+
--- main.py
+++ main.py
@@ -1,53 +1,5 @@
-# from funcoes import*
+from funcoes import*
 
-# idade = input('Digite a idade do entrevistado')
-
-# idade_candidato(idade)
-
-from datetime import datetime #biblioteca para executar data e hora.
-
-idade_dos_candidatos = []
-genero_dos_candidatos = []
-pergunta01 = []
-pergunta02 = []
-pergunta03 = []
-pergunta04 = []
-data_e_hora_da_resposta = []
-
-
-        
-while True:
-    idade_candidato = input('Qual a idade do candidato?')
-    idade_dos_candidatos.append(idade_candidato)
-    if idade_candidato == '00':
-        break
-    print('m = masculino e f = feminino')
-    genero_candidato = input('Qual o gênero do candidato? ')
-    genero_dos_candidatos.append(genero_candidato)
-    print('Sim (1), Não (2), Não sei responder (3)?')
-    pergunta_01 = input('Compraria um carro pela internet?')
-    pergunta01.append(pergunta_01)
-    print('Sim (1), Não (2), Não sei responder (3)?')
-    pergunta_02 = input('Se o preço do carro fosse 15% abaixo do valor de mercado, você compraria?')
-    pergunta02.append(pergunta_02)
-    print('Sim (1), Não (2), Não sei responder (3)?')
-    pergunta_03 = input('Tem intenção de comprar um carro nos próximos 6 meses?')
-    pergunta03.append(pergunta_03)
-    print('Sim (1), Não (2), Não sei responder (3)?')
-    pergunta_04 = input('Gostaria do serviço de despachante na compra do seu carro?')
-    pergunta04.append(pergunta_04)
-    hora_atual = datetime.now() #Registra o horário 
-    data_e_hora_atual = hora_atual.strftime('%d/%m/%Y %H:%M') #registra a data e implementa o horário da variável 'hora atual'.
-    data_e_hora_da_resposta.append(data_e_hora_atual)
-
-print(idade_dos_candidatos,
-      genero_dos_candidatos,
-      pergunta01,
-      pergunta02,
-      pergunta03,
-      pergunta04, 
-      data_e_hora_da_resposta)
-
-for item1, item2, item3, item4, item5, item6, item7 in zip(idade_dos_candidatos,genero_dos_candidatos,pergunta01,
-pergunta02,pergunta03,pergunta04, data_e_hora_da_resposta):
-    print(f'{item1} {item2} {item3} {item4} {item5} {item6} {item7}')
+questionario = Perguntas()
+questionario.obter_resposta()
+questionario.add_lista()
