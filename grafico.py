d0b95bf27f9a88be04e3deaf71512fa7218d5135
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-06-06 15:09:43 -0300

    Pequena correção
    Corrigir os comentários do código
    Adição dos print trello

--- grafico.py
+++ grafico.py
@@ -1,40 +1,3 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# # Ler o arquivo CSV e criar o dfFrame
-# pergunta = 'Pergunta1'
-# nome_arquivo = 'dados.csv'
-# df = pd.read_csv(nome_arquivo)
-
-# # Definir as faixas etárias
-# faixas = [18, 30, 45, 100]  # Exemplo de faixas: 18-30, 30-45, 45+
-
-# # Definir os rótulos das faixas etárias
-# rotulos = ['18-30', '30-45', '45+']
-
-# # Agrupar as idades em faixas
-# df['Faixa Etária'] = pd.cut(df['Idade'], bins=faixas, labels=rotulos, right=False)
-
-# # Calcular as porcentagens de "Sim" e "Não" para cada faixa etária
-# porcentagens = df.groupby('Faixa Etária')[pergunta].value_counts(normalize=True) * 100
-# porcentagens = porcentagens.unstack().fillna(0)
-
-# # Criar o gráfico de barras
-# ax = porcentagens.plot(kind='bar', stacked=True)
-
-# # Adicionar as porcentagens nas barras
-# for container in ax.containers:
-#     ax.bar_label(container, fmt='%.1f%%', label_type='edge')
-
-# # Definir os rótulos dos eixos
-# plt.xlabel('Faixa Etária')
-# plt.ylabel('Porcentagem')
-
-# # Definir o título do gráfico
-# plt.title(f'Relação entre Faixa Etária e Resposta para {pergunta}')
-
-# # Exibir o gráfico
-# plt.show()
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -43,24 +6,6 @@ pergunta = 'Pergunta1'
 nome_arquivo = 'dados.csv'
 df = pd.read_csv(nome_arquivo)
 df = pd.DataFrame(df)
-
-# Agrupando as respostas da pergunta 1 em duas categorias
-# df['Resposta'] = df['Pergunta1'].apply(lambda x: 'Sim' if x == 'Sim' else 'Não')
-
-# # Agrupando as respostas da pergunta 1 por faixa etária
-# counts = df.groupby('Idade')['Pergunta1'].value_counts().unstack()
-
-# # Plotando o gráfico de barras
-# counts.plot(kind='bar', stacked=True)
-
-# # Adicionando rótulos e título ao gráfico
-# plt.xlabel('Idade')
-# plt.ylabel('Contagem')
-# plt.title('Distribuição das Respostas à Pergunta 1 por Faixa Etária')
-
-# # Exibindo o gráfico
-# plt.show()
-
 
 faixas_etarias = ['18-30', '30-40', '40-50', '50-60']
 
