136133578e32b276c2082862d1ed147d669b8b4d
Wdson Danrlei <wdson91@gmail.com>
2023-06-16 15:16:17 -0300

    atualização do bot

--- auto.py
+++ auto.py
@@ -8,38 +8,33 @@ import time
 
 genero_lista = ['1', '2', '3']
 opcoes_validas = ['1', '2', '3']
-
+lista2 = []
 def escrever(dados): 
-# Verifica se o arquivo CSV já existe
     nome_arquivo = 'dados.csv'
     arquivo_existe = os.path.isfile(nome_arquivo)
-
+    
     # Dados a serem adicionados ao arquivo CSV
-    novos_dados = {'Idade': [dados[0][0]], 'Genero': [dados[0][1]], 'Pergunta1': [dados[0][2]], 'Pergunta2': [dados[0][3]], 'Pergunta3': [dados[0][4]], 'Pergunta4': [dados[0][5]], 'Data/Hora': [dados[0][6]]}
-
+    for i in range(len(dados)):
+        novos_dados = {'Idade': [dados[i][0][0]], 'Genero': [dados[i][0][1]], 'Pergunta1': [dados[i][0][2]], 'Pergunta2': [dados[i][0][3]], 'Pergunta3': [dados[i][0][4]], 'Pergunta4': [dados[i][0][5]], 'Data/Hora': [dados[i][0][6]]}
     # Se o arquivo já existe, carrega o conteúdo existente
-    if arquivo_existe:
-        # Carrega o arquivo CSV existente para um DataFrame
-        df_existente = pd.read_csv(nome_arquivo)
+        if arquivo_existe:
+            # Carrega o arquivo CSV existente para um DataFrame
+            df_existente = pd.read_csv(nome_arquivo)
 
-        # Adiciona os novos dados ao DataFrame existente
-        df_novo = pd.DataFrame(novos_dados)
-        df_final = pd.concat([df_existente, df_novo], ignore_index=True)
+            # Adiciona os novos dados ao DataFrame existente
+            df_novo = pd.DataFrame(novos_dados)
+            df_final = pd.concat([df_existente, df_novo], ignore_index=True)
 
-        # Salva o DataFrame atualizado de volta para o arquivo CSV
-        df_final.to_csv(nome_arquivo, index=False)
-        print("\nSalvando Pesquisa...")
-        time.sleep(1)
-        print('Pesquisa salva com sucesso!\n')
-        
-    else:
-        # Se o arquivo não existe, cria um novo arquivo CSV com os novos dados
-        df_novo = pd.DataFrame(novos_dados)
-        df_novo.to_csv(nome_arquivo, index=False)
-        print("\nSalvando Pesquisa...")
-        time.sleep(1)
-        print('Pesquisa salva com sucesso!\n')
-        
+            # Salva o DataFrame atualizado de volta para o arquivo CSV
+            df_final.to_csv(nome_arquivo, index=False)
+        else:
+            # Se o arquivo não existe, cria um novo arquivo CSV com os novos dados
+            df_novo = pd.DataFrame(novos_dados)
+            df_novo.to_csv(nome_arquivo, index=False)
+    print("\nSalvando Pesquisa...\n")        
+    time.sleep(1)
+    print('Pesquisa salva com sucesso!\n')
+    
 for i in range(5):
     idade = random.randint(18, 60)
     genero = random.choice(genero_lista)
@@ -67,8 +62,15 @@ for i in range(5):
 
     # Aqui você pode chamar a função 'escrever(dados)' para salvar as respostas no arquivo CSV
     questionario = Perguntas(idade, genero, p1, p2, p3, p4)
-
-    dados = questionario.add_lista() 
-    print(dados)
-    escrever(dados)
     
+    dados = questionario.add_lista() 
+    
+    questionario = Perguntas(idade, genero, p1, p2, p3, p4)
+    
+    dados = questionario.add_lista() 
+    
+    lista2.append(dados)
+    print('\n')
+    print("Obrigado por participar da pesquisa!")
+    print('\n')
+escreverCsv(lista2)   
